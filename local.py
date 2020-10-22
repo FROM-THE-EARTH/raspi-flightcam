f96f939be20ef2f6c1e3ee809d4595daf0b4bc15
jjj999 <spaceship2021@gmail.com>
2020-10-22 17:24:47 +0900

    enable to execute as a cml tools

--- local.py
+++ local.py
@@ -8,27 +8,30 @@ This script make your debug faster.
 
 from os.path import join, expanduser, abspath, exists
 import shutil
+import sys
 
 
-path_pisat = abspath("camera")           # TO EDIT as the pisat package's path in your environment
-name_env = "nse2020-hwof8Sus"            # TO EDIT as the virtual environment's name in your environment
-version_python = "3.8"
-path_site = join(expanduser("~"),
-                 ".local",
-                 "share",
-                 "virtualenvs",
-                 name_env,
-                 "lib",
-                 "python" + version_python, 
-                 "site-packages", 
-                 "camera")
-
-
-def main():
+def main(path_pkg: str, path_site: str):
     if exists(path_site):
         shutil.rmtree(path_site)
 
-    shutil.copytree(path_pisat, path_site)
+    shutil.copytree(path_pkg, path_site)
+    
     
 if __name__ == "__main__":
-    main()
+    
+    name_pkg = sys.argv[1]
+    path_pkg = abspath(name_pkg)
+    name_env = sys.argv[2]
+    version_python = "3.8"
+    path_site = join(expanduser("~"),
+                    ".local",
+                    "share",
+                    "virtualenvs",
+                    name_env,
+                    "lib",
+                    "python" + version_python, 
+                    "site-packages", 
+                    name_pkg)
+
+    main(path_pkg, path_site)
